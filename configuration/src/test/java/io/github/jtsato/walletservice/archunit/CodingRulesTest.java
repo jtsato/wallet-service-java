b672bf84886d9ca354ed3db43550930832bba09d
unknown <jorge.sato@viavarejo.com.br>
2025-02-15 12:40:10 -0300

    Initial Commit

--- configuration/src/test/java/io/github/jtsato/walletservice/archunit/CodingRulesTest.java
+++ configuration/src/test/java/io/github/jtsato/walletservice/archunit/CodingRulesTest.java
@@ -20,34 +20,33 @@ import static com.tngtech.archunit.library.GeneralCodingRules.*;
 public class CodingRulesTest {
 
     @ArchTest
-    private final ArchRule no_access_to_standard_streams = NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
+    private final ArchRule noAccessToStandardStreams = NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
 
     @ArchTest
-    private void no_access_to_standard_streams_as_method(final JavaClasses classes) {
+    private void noAccessToStandardStreams(final JavaClasses classes) {
         noClasses().should(ACCESS_STANDARD_STREAMS).check(classes);
     }
 
     @ArchTest
-    private final ArchRule no_generic_exceptions = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
+    private final ArchRule noGenericExceptions = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
 
     @ArchTest
-    private final ArchRule no_java_util_logging = NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
+    private final ArchRule noJavaUtilLogging = NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
 
     @ArchTest
-    private final ArchRule loggers_should_be_private_static_final = fields().that()
-                                                                            .haveRawType(Logger.class)
-                                                                            .should()
-                                                                            .bePrivate()
-                                                                            .andShould()
-                                                                            .beStatic()
-                                                                            .andShould()
-                                                                            .beFinal()
-                                                                            .because("we agreed on this convention");
+    private final ArchRule loggersShouldBePrivateStaticFinal = fields().that()
+                                                                       .haveRawType(Logger.class)
+                                                                       .should()
+                                                                       .bePrivate()
+                                                                       .andShould()
+                                                                       .beStatic()
+                                                                       .andShould()
+                                                                       .beFinal()
+                                                                       .because("we agreed on this convention");
+    @ArchTest
+    private final ArchRule noJodaTime = NO_CLASSES_SHOULD_USE_JODATIME;
 
     @ArchTest
-    private final ArchRule no_jodatime = NO_CLASSES_SHOULD_USE_JODATIME;
-
-    @ArchTest
-    static final ArchRule no_classes_should_access_standard_streams_or_throw_generic_exceptions = CompositeArchRule.of(NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS)
-                                                                                                                   .and(NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS);
+    static final ArchRule noClassesShouldAccessStandardStreamsOrThrowGenericExceptions = CompositeArchRule.of(NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS)
+                                                                                                        .and(NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS);
 }
