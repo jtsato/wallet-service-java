25c44f8ea0982d2425628d10ef5fc2eaa1c1992b
unknown <jorge.sato@viavarejo.com.br>
2025-02-15 12:45:09 -0300

    Initial Commit

--- infra/database/src/main/java/io/github/jtsato/walletservice/infra/domains/wallet/WalletMapper.java
+++ infra/database/src/main/java/io/github/jtsato/walletservice/infra/domains/wallet/WalletMapper.java
@@ -2,10 +2,13 @@ package io.github.jtsato.walletservice.infra.domains.wallet;
 
 import io.github.jtsato.walletservice.core.domains.wallet.model.Wallet;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface WalletMapper {
 
+    @Mapping(target = "withBalance", ignore = true)
+    @Mapping(target = "withUpdatedAt", ignore = true)
     Wallet of(final WalletEntity walletEntity);
     WalletEntity of(final Wallet wallet);
 }
