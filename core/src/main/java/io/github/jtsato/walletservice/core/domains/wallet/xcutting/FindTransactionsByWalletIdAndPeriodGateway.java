1a4c744fa0af260688e7ccbec4b09ecf84b5587b
unknown <jorge.sato@viavarejo.com.br>
2025-02-16 03:20:17 -0300

    Initial Commit

--- core/src/main/java/io/github/jtsato/walletservice/core/domains/wallet/xcutting/FindTransactionsByWalletIdAndPeriodGateway.java
+++ core/src/main/java/io/github/jtsato/walletservice/core/domains/wallet/xcutting/FindTransactionsByWalletIdAndPeriodGateway.java
@@ -1,11 +1,12 @@
-package io.github.jtsato.walletservice.core.domains.wallet.usecase.history;
+package io.github.jtsato.walletservice.core.domains.wallet.xcutting;
 
 import io.github.jtsato.walletservice.core.domains.transactions.model.Transaction;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface HistoryUseCase {
+@FunctionalInterface
+public interface FindTransactionsByWalletIdAndPeriodGateway {
 
     List<Transaction> execute(final Long walletId, final LocalDateTime initialDate, final LocalDateTime finalDate);
 }
