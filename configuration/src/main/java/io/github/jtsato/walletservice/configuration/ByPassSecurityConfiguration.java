304926653307f77fe3e35a97a534b1a9590459a4
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2026-07-30 22:46:50 -0300

    fix(sonar): fix issue findings

--- configuration/src/main/java/io/github/jtsato/walletservice/configuration/ByPassSecurityConfiguration.java
+++ configuration/src/main/java/io/github/jtsato/walletservice/configuration/ByPassSecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
